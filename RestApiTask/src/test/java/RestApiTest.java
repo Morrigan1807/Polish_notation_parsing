152ee171499baa2558cbf0dfb49daca854331e5d
Morrigan <werty18071997@gmail.com>
2021-05-05 15:25:59 +0300

    Initial commit.

--- RestApiTask/src/test/java/RestApiTest.java
+++ RestApiTask/src/test/java/RestApiTest.java
@@ -3,10 +3,11 @@ import com.jayway.restassured.filter.log.RequestLoggingFilter;
 import com.jayway.restassured.filter.log.ResponseLoggingFilter;
 import com.jayway.restassured.http.ContentType;
 import com.jayway.restassured.response.Response;
+import com.jayway.restassured.specification.RequestSpecification;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 
-import java.io.*;
 import java.util.Objects;
 
 import static com.jayway.restassured.RestAssured.given;
@@ -14,82 +15,81 @@ import static com.jayway.restassured.RestAssured.given;
 public class RestApiTest {
 
     public static String accessToken;
+    public static RequestSpecification setAccessToken;
+    public static String uploadedFileId;
+    public static String copiedFileId;
 
     @BeforeAll
     public static void generateAccessToken() {
         RestAssured.filters(new RequestLoggingFilter(), new ResponseLoggingFilter());
         accessToken = given().contentType(ContentType.JSON).post(Constant.ACCESS_TOKEN_CREATED).then().extract().jsonPath().getString(Constant.CSS_ACCESS_TOKEN);
+        setAccessToken = given().auth().oauth2(accessToken);
     }
 
     @Test
-    public void uploadPicture() {
-        given().auth().oauth2(accessToken).contentType("multipart/form-data")
-                .multiPart("jsonParams", new File(Objects.requireNonNull(TestUtil.getFileURIFromResources("param.json"))), "application/json")
-                .multiPart("picFile", new File(Objects.requireNonNull(TestUtil.getFileURIFromResources("pic.jpg"))), "image/jpeg")
-                .when().post("https://www.googleapis.com/upload/drive/v3/files").then().assertThat().statusCode(200);
+    @Order(1)
+    public void getFileList() {
+        String getFileListUrl = "https://www.googleapis.com/drive/v3/files/";
+        setAccessToken.when().get(getFileListUrl).then().assertThat().statusCode(200);
     }
 
     @Test
+    @Order(2)
+    public void uploadPicture() {//TODO get id
+        Response uploadPictureResponse = setAccessToken.contentType("multipart/form-data")
+                .multiPart("jsonParams", Objects.requireNonNull(TestUtil.getFileURIFromResources("param.json")), "application/json")
+                .multiPart("picFile", Objects.requireNonNull(TestUtil.getFileURIFromResources("pic.jpg")), "image/jpeg")
+                .when().post("https://www.googleapis.com/upload/drive/v3/files");
+        uploadedFileId = uploadPictureResponse.then().extract().jsonPath().getString("id");
+        uploadPictureResponse.then().assertThat().statusCode(200);
+    }
+
+    @Test
+    @Order(3)
+    public void getFileContent() {
+        String getFileContentUrl = String.format("https://www.googleapis.com/drive/v3/files/%s", uploadedFileId);
+        Response response = setAccessToken.queryParam("alt", "media").when().get(getFileContentUrl);
+        response.then().assertThat().statusCode(200);
+
+        TestUtil.saveResponseToFile(response);
+    }
+
+    @Test
+    @Order(4)
     public void renameFile() {
-        String fileId = "1GyZ0tKbpAGsDbI0uUbHmrWrq0-MfYff1";
-        String renameFileUrl = String.format("https://www.googleapis.com/drive/v3/files/%s", fileId);
-        given().auth().oauth2(accessToken).contentType(ContentType.JSON).body("{\"name\":\"pic.png\"}")
+        String renameFileUrl = String.format("https://www.googleapis.com/drive/v3/files/%s", uploadedFileId);
+        setAccessToken.contentType(ContentType.JSON).body("{\"name\":\"pic.png\"}")
                 .when().patch(renameFileUrl).then().assertThat().statusCode(200);
     }
 
     @Test
-    public void copyFile() {
-        String fileId = "1GyZ0tKbpAGsDbI0uUbHmrWrq0-MfYff1";
-        String copyFileUrl = String.format("https://www.googleapis.com/drive/v3/files/%s/copy", fileId);
-        given().auth().oauth2(accessToken).when().post(copyFileUrl).then().assertThat().statusCode(200);
+    @Order(5)
+    public void getFileData() {
+        String getFileContentUrl = String.format("https://www.googleapis.com/drive/v3/files/%s", uploadedFileId);
+        setAccessToken.when().get(getFileContentUrl).then().assertThat().statusCode(200);
     }
 
     @Test
+    @Order(6)
+    public void copyFile() {
+        String copyFileUrl = String.format("https://www.googleapis.com/drive/v3/files/%s/copy", uploadedFileId);
+        Response response = setAccessToken.when().post(copyFileUrl);
+        copiedFileId = response.then().extract().jsonPath().getString("id");
+        response.then().assertThat().statusCode(200);
+    }
+
+    @Test
+    @Order(7)
     public void deleteFile() {
-        String fileId = "1bN29_DJbYELMXxo1yohtBet48e2xSyGd";
-        String deleteFileUrl = String.format("https://www.googleapis.com/drive/v3/files/%s", fileId);
+        String deleteFileUrl = String.format("https://www.googleapis.com/drive/v3/files/%s", uploadedFileId);
         given().auth().oauth2(accessToken).when().delete(deleteFileUrl).then().assertThat().statusCode(204);
     }
 
     @Test
+    @Order(8)
     public void replaceFile() {
-        String fileId = "1GyZ0tKbpAGsDbI0uUbHmrWrq0-MfYff1";
-        String replaceFileUrl = String.format("https://www.googleapis.com/drive/v3/files/%s", fileId);
+        String replaceFileUrl = String.format("https://www.googleapis.com/drive/v3/files/%s", copiedFileId);
         given().auth().oauth2(accessToken).contentType(ContentType.JSON).body("{\"trashed\":true}")
                 .when().patch(replaceFileUrl).then().assertThat().statusCode(200);
-    }
-
-    @Test
-    public void getFileContent() {
-        String fileId = "11NPlJ1O-Fi17jK7SnM_8PKR5A70deXEv";
-        String getFileContentUrl = String.format("https://www.googleapis.com/drive/v3/files/%s", fileId);
-        Response response = given().auth().oauth2(accessToken).queryParam("alt", "media").when().get(getFileContentUrl);
-        response.then().assertThat().statusCode(200);
-
-        saveResponseToFile(response);
-    }
-
-    private void saveResponseToFile(Response response) {
-        try {
-            File fileToSaveTo = new File("src/test/resources/downloadedFile.jpg");
-            byte[] responseBodyAsByteArray = response.asByteArray();
-            OutputStream outStream = new FileOutputStream(fileToSaveTo);outStream.write(responseBodyAsByteArray);
-            outStream.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    @Test
-    public void getFileList() {
-        String getFileListUrl = "https://www.googleapis.com/drive/v3/files/";
-        given().auth().oauth2(accessToken).when().get(getFileListUrl).then().assertThat().statusCode(200);
-    }
-
-    @Test
-    public void getFileData() {
-        String fileId = "11NPlJ1O-Fi17jK7SnM_8PKR5A70deXEv";
-        String getFileContentUrl = String.format("https://www.googleapis.com/drive/v3/files/%s", fileId);
-        given().auth().oauth2(accessToken).when().get(getFileContentUrl).then().assertThat().statusCode(200);
     }
 }
--- RestApiTask/src/test/java/TestUtil.java
+++ RestApiTask/src/test/java/TestUtil.java
@@ -1,15 +1,32 @@
+import com.jayway.restassured.response.Response;
+
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
 import java.net.URISyntaxException;
 import java.util.Objects;
 
 public class TestUtil {
 
-    public static String getFileURIFromResources(String fileName) {
+    public static File getFileURIFromResources(String fileName) {
         try {
-            return (Objects.requireNonNull(TestUtil.class.getClassLoader().getResource(fileName))).toURI().getPath();
+            return new File((Objects.requireNonNull(TestUtil.class.getClassLoader().getResource(fileName))).toURI());
         } catch (URISyntaxException e) {
             e.printStackTrace();
             return null;
+        }
+    }
+
+    public static void saveResponseToFile(Response response) {
+        try {
+            File fileToSaveTo = new File("src/test/resources/downloadedFile.jpg");
+            byte[] responseBodyAsByteArray = response.asByteArray();
+            OutputStream outStream = new FileOutputStream(fileToSaveTo);
+            outStream.write(responseBodyAsByteArray);
+            outStream.close();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
